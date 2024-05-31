61db34709bff9f48d13eeb2d4f4162de3e6bea5f
Josh Holbrook <josh.holbrook@gmail.com>
2024-05-31 11:01:50 -0800

    link and unlink

--- cmd/link.go
+++ cmd/link.go
@@ -6,6 +6,9 @@ package cmd
 import (
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+
+	"github.com/jfhbrook/dosapp/application"
+	"github.com/jfhbrook/dosapp/config"
 )
 
 var linkCmd = &cobra.Command{
@@ -13,12 +16,24 @@ var linkCmd = &cobra.Command{
 	Short: "Create a script that starts the app",
 	Long:  `Create a script in DOSAPP_LINK_HOME that starts the application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Info().Msg("TODO: pull app name from args")
-		log.Info().Msg("TODO: require that the app is installed")
-		log.Info().Msg("TODO: refresh main configuration")
-		log.Info().Msg("TODO: init app configuration")
-		log.Info().Msg("TODO: refresh app configuration")
-		log.Info().Msg("TODO: run link task")
+		appName := args[0]
+		conf := config.NewConfig()
+
+		refreshFlag, _ := cmd.Flags().GetBool("refresh")
+
+		app := application.NewApp(conf, appName)
+
+		if !app.Exists() {
+			log.Fatal().Msgf("%s not found. Did you install it?", appName)
+		}
+
+		if refreshFlag {
+			if err := app.Refresh(); err != nil {
+				log.Panic().Err(err).Msg("Failed to refresh application config")
+			}
+		}
+
+		app.Run("link")
 	},
 }
 
