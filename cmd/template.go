63f0690a8ad2ac3f6b86bd037ff2f0fefac6a00c
Josh Holbrook <josh.holbrook@gmail.com>
2024-06-02 10:56:22 -0800

    handle link templates

--- cmd/template.go
+++ cmd/template.go
@@ -23,13 +23,15 @@ var templateCmd = &cobra.Command{
 	Long:  `Render a config or package template.`,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		templateName := args[0]
+		templatePath := args[0]
 		conf := config.NewConfig()
 
 		configFlag, _ := cmd.Flags().GetBool("config")
+		linkFlag, _ := cmd.Flags().GetBool("link")
 		packageName := cmd.Flag("package").Value.String()
 
-		basename := strings.TrimSuffix(templateName, filepath.Ext(templateName))
+		templateName := filepath.Base(templatePath)
+		fileName := strings.TrimSuffix(templateName, filepath.Ext(templateName))
 
 		var env map[string]string
 		var tmpl *template.Template
@@ -40,10 +42,29 @@ var templateCmd = &cobra.Command{
 		} else if packageName != "" {
 			app := application.NewApp(conf, packageName)
 			pkg := packages.NewPackage(conf, packageName)
-			src := filepath.Join(pkg.Path(), templateName)
+			src := filepath.Join(pkg.Path(), templatePath)
 
 			env = app.Env()
-			dest = filepath.Join(app.Path(), basename)
+
+			if linkFlag {
+				linkPath, err := filepath.Rel("bin", templatePath)
+
+				if err != nil {
+					log.Panic().Err(err).Msg("Failed to get relative path")
+				}
+
+				linkPath = strings.TrimSuffix(linkPath, filepath.Ext(linkPath))
+
+				dest = filepath.Join(conf.LinkHome, linkPath)
+			} else {
+				dest = filepath.Join(app.Path(), fileName)
+			}
+
+			log.Debug().Str(
+				"name", templateName,
+			).Str(
+				"src", src,
+			).Msg("Parsing package template")
 
 			var err error
 
@@ -53,11 +74,20 @@ var templateCmd = &cobra.Command{
 				log.Fatal().Err(err).Msg("Failed to parse package template")
 			}
 		} else if configFlag {
+			if linkFlag {
+				log.Fatal().Msg("Cannot link a config template")
+			}
 			env = conf.Env()
-			dest = filepath.Join(conf.ConfigHome, templateName)
+			dest = filepath.Join(conf.ConfigHome, templatePath)
+
+			log.Debug().Str(
+				"name", templateName,
+			).Str(
+				"src", templatePath,
+			).Msg("Parsing config template")
 
 			var err error
-			tmpl, err = template.New(templateName).Parse(config.Templates[templateName])
+			tmpl, err = template.New(templateName).Parse(config.Templates[templatePath])
 
 			if err != nil {
 				log.Panic().Err(err).Msg("Failed to parse config template")
@@ -65,6 +95,10 @@ var templateCmd = &cobra.Command{
 		} else {
 			log.Fatal().Msg("Must specify either a package or a config template")
 		}
+
+		log.Debug().Str(
+			"destination", dest,
+		).Msg("Rendering template")
 
 		var f *os.File
 		var err error
@@ -93,6 +127,7 @@ func init() {
 
 	templateCmd.Flags().StringP("package", "p", "", "Package that contains template")
 	templateCmd.Flags().BoolP("config", "C", false, "Use a config template")
+	templateCmd.Flags().BoolP("link", "L", false, "Template is a link")
 
 	// Here you will define your flags and configuration settings.
 
